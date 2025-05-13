316a5cee0d1a507b21126856bf104d42b0399b0a
integer_ndegwa <allancuea@gmail.com>
2025-05-13 13:47:40 +0300

    Delete frontend directory

--- frontend/src/contract.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import abi from "./PlasticSokoABI.json";
-
-const CONTRACT_ADDRESS = "0xF6474B8281C7C2c6860Bc89bC2e68bfD7A67b021";
-
-export function getContract(providerOrSigner) {
-  return new ethers.Contract(CONTRACT_ADDRESS, abi, providerOrSigner);
-}
